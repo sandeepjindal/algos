9dd42e0e143ae2a3e81bf6dfc81d1065184d0fd0
Sandeep jindal <jindalsandeep47@gmail.com>
2020-02-09 19:28:47 +0700

    Update splitting_a_string.cpp

--- common_syntax/c++/splitting_a_string.cpp
+++ common_syntax/c++/splitting_a_string.cpp
@@ -1,7 +1,9 @@
-    std::stringstream ss(str);
-
-
-    std::string s;
-    while (std::getline(ss, s, delim)) {
-        out.push_back(s);
+    vector<string> splitter(string s,char delim){
+        vector<string> out;
+        std::stringstream ss(str);
+        std::string s;
+        while (std::getline(ss, s, delim)) {
+            out.push_back(s);
+        }
+        return out;
     }
