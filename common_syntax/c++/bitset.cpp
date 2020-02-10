14b037820f2c9de53fb79269d1a64e1f6a2b94c5
Sandeep jindal <jindalsandeep47@gmail.com>
2020-02-10 11:23:20 +0700

    Create bitset.cpp

---
+++ common_syntax/c++/bitset.cpp
@@ -0,0 +1,16 @@
+  
+int main() {
+  int num = 7;
+  cout << bitset<32>(num).count()<< endl;
+
+  std::bitset<5> b(5); 
+  std::cout << b.to_ulong() << '\n';
+
+  std::string alpha_bit_string = "aBaaBBaB"; 
+  std::bitset<8> b1(alpha_bit_string, 0, alpha_bit_string.size(), 
+                      'a', 'B');         // [0,1,0,0,1,1,0,1] 
+  
+  std::cout << b1 << '\n'; 
+
+
+}
