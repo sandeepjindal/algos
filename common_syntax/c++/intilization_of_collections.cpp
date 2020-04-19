275a125cc72796ee0efc406555350700340dcb89
Sandeep jindal <jindalsandeep47@gmail.com>
2020-04-19 10:20:11 +0700

    Update intilization_of_collections.cpp

--- common_syntax/c++/intilization_of_collections.cpp
+++ common_syntax/c++/intilization_of_collections.cpp
@@ -4,3 +4,11 @@ std::map<std::string, int> mapOfMarks = {
 		{"Riti",2},
 		{"Jack",4}
 };
+
+
+int m = 2; //size of vector
+vector<int> marks(m,0); // {0,0}
+
+// 2d-vector initialization
+int n=3; // classes
+vector<vector<int>> classMarks(n,vector<int>(m,0))
