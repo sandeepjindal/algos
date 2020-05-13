d991c3118d17257f04374c6a2af4c1801b87761c
Sandeep jindal <jindalsandeep47@gmail.com>
2020-05-13 13:37:29 +0700

    Update and rename [priority_queue]_minheap.cpp to [priority_queue]_min_n_max_heap.cpp

---
+++ common_syntax/c++/[priority_queue]_min_n_max_heap.cpp
@@ -0,0 +1,22 @@
+// Max Heap
+ priority_queue<int> pq;
+ // operations
+ pq.push(5); 
+ pq.push(1); 
+ pq.push(10);
+
+ pq.pop()
+
+//Min Heap
+ priority_queue <int, vector<int>, greater<int> > pq;
+
+
+// this example is with pair 
+priority_queue<pi, vector<pi>, greater<pi> > pq; 
+  
+    pq.push(make_pair(10, 200)); 
+    pq.push(make_pair(20, 100)); 
+    pq.push(make_pair(15, 400)); 
+  
+    pair<int, int> top = pq.top(); 
+    cout << top.first << " " << top.second; 
