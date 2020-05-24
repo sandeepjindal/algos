77e15084854380277d43d7477505fc6843de8aaf
Sandeep jindal <jindalsandeep47@gmail.com>
2020-05-24 10:39:26 +0700

    Update [string]_substrings.cpp

--- common_syntax/c++/[string]_substrings.cpp
+++ common_syntax/c++/[string]_substrings.cpp
@@ -7,3 +7,11 @@
   std::string str3 = str.substr (pos);     // get from "live" to the end
 
   std::cout << str2 << ' ' << str3 << '\n';
+
+
+// to check if substring is prefix of string or not
+
+   auto res = std::mismatch(searchWord.begin(), searchWord.end(), words[i].begin());
+   if(res.first == searchWord.end()){
+      // yes it is
+   }
