dd5ca069bb4e4b7bb2e458425412716b292f116c
Sandeep jindal <jindalsandeep47@gmail.com>
2024-07-27 13:34:13 +0200

    Create Reader.java

---
+++ common_syntax/java/Reader.java
@@ -0,0 +1,10 @@
+// Scanner
+
+Scanner sc = new Scanner(System.in)
+sc.nextInt()
+sc.close()
+
+// BufferedReader
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+br.readLine()
+br.close()
