59d57c9a2abf31e716b2f51bead80d2162ba46fb
Sandeep jindal <jindalsandeep47@gmail.com>
2024-09-21 19:51:32 +0200

    Create length.java

---
+++ common_syntax/java/length.java
@@ -0,0 +1,10 @@
+String str = "Hello, world!";
+int strLength = str.length(); // 13
+
+int[] arr = {1, 2, 3, 4, 5};
+int arrLength = arr.length; // 5
+
+ArrayList<String> list = new ArrayList<>();
+list.add("Apple");
+list.add("Banana");
+int listSize = list.size(); // 2
