9374becbea7f128d6f91c857c0665b1f26f059f5
Sandeep jindal <jindalsandeep47@gmail.com>
2024-09-22 19:13:33 +0200

    Update ArrayList_x_array.java

--- common_syntax/java/ArrayList_x_array.java
+++ common_syntax/java/ArrayList_x_array.java
@@ -1,5 +1,8 @@
 // arraylist to int[]
 arrayList.stream().mapToInt(Integer::intValue).toArray();
 
+// max of array
+arrayList.stream().max().orElse(Integer::MIN_VALUE);
+
 // int[] -> arrayList       
 List<Integer> arrayList = Arrays.stream(intArray).boxed().collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
