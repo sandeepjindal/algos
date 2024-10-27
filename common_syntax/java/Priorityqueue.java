777bf5307abbb0e9112cec8e5ec6d075b502dd3b
Sandeep jindal <jindalsandeep47@gmail.com>
2024-10-27 11:00:37 +0100

    Update Priorityqueue.java

--- common_syntax/java/Priorityqueue.java
+++ common_syntax/java/Priorityqueue.java
@@ -6,10 +6,14 @@
                 return i2.compareTo(i1);
             }
         });
-
+        // alternative 1
         //         Queue<Integer> pq = new PriorityQueue<>( (i1, i2) -> {
-       //     return i2-i1;
-       // });
+        //     return i2-i1;
+        // });
+
+        // alternative 2
+        //        Queue<Pair<Integer, Integer>> pq = new PriorityQueue<Pair<Integer,Integer>>
+        //    (Comparator.comparing(Pair::getKey));
 
         priorityQueue.offer(10);   
 
